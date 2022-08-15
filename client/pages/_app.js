c29d321ef403e79a91eb2e75e75454456989f13f
surajondev <surajvishwakarma625@gmail.com>
2022-08-15 12:38:38 +0530

    adding ga

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import Script from 'next/script'
 
 const theme = extendTheme({
   colors: {
@@ -17,6 +18,22 @@ function MyApp({ Component, pageProps }) {
       <ChakraProvider theme={theme}>
         <div className="background">
           <Component {...pageProps} />
+          <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+
+          <Script id="google-analytics" strategy="lazyOnload">
+            {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                      page_path: window.location.pathname,
+                    });
+                        `}
+          </Script>
+
         </div>
       </ChakraProvider>
     </ThirdwebProvider>
